152d9ee6915dfb757ae795050bf1fbcf00aa0b04
Eyad-Hantouli <eyadhantouli0@gmail.com>
2024-02-09 15:11:28 +0300

    Initial commit

---
+++ src/test/java/com/codevision/codevision/CodeVisionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codevision.codevision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeVisionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
